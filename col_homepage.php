6d1000b175a868f433317d48d6e3840582b26629
Sanjay-sundaresan <creativeid.03@gmail.com>
2021-05-23 00:31:09 +0530

    Collector frontend landing page is done
    Hyperlinks are yet to be filled

---
+++ col_homepage.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- =====BOX ICONS===== -->
+        <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+
+        <!-- ===== CSS ===== -->
+        <link rel="stylesheet" href="css/col_homepagestyle.css">
+
+        <title>E-Waste collector portal</title>
+    </head>
+    <body>
+        <!--===== HEADER =====-->
+        <header class="l-header">
+            <nav class="nav bd-grid">
+                
+                <div class="nav__toggle" id="nav-toggle">
+                    <i class='bx bx-menu'></i>
+                </div>
+
+                <div class="nav__menu" id="nav-menu">
+                    <div class="nav__close" id="nav-close">
+                        <i class='bx bx-x'></i>
+                    </div>
+
+                </div>
+            </nav>
+        </header>
+
+        <main class="l-main">
+            <!--===== HOME =====-->
+            <section class="home" id="home">
+                <div class="home__container bd-grid">
+                    <div class="home__img">
+                        <img src="img/img1.png" alt="" data-speed="-2" class="move">
+                        <img src="img/img2.png" alt="" data-speed="2" class="move">
+                        <img src="img/img3.png" alt="" data-speed="2" class="move">
+                        <img src="img/img4.png" alt="" data-speed="-2" class="move">
+                        <img src="img/img5.png" alt="" data-speed="-2" class="move">
+                        
+                    </div>
+
+                    <div class="home__data">
+                        <h1 class="home__title">Welcome<br> to E-Waste <br> Collector portal</h1>
+                        <p class="home__description">Communicate with State-level recyclers <br> and track E-Waste bins<br>in your pincode.</p>
+                        <a href="#" class="home__button">Track Bins</a>
+                        <a href="#" class="home__button">Specify Shipment details</a>
+                        
+                    </div>
+                </div>
+            </section>
+        </main>
+
+        <!--===== GSAP =====-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
+
+        <!--===== MAIN JS =====-->
+        <script src="js/col_js.js"></script>
+    </body>
+</html>
