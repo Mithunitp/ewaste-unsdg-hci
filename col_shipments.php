bdcb2fceabf642b1646bb1274c1a69ea25a819ef
Jay Gupta <jaygupta.id@gmail.com>
2021-05-24 11:25:29 +0530

    Fine tuning in collector pages

--- col_shipments.php
+++ col_shipments.php
@@ -19,6 +19,28 @@
         <link rel="stylesheet" href="css/col_homepagestyle.css">
 
         <title>E-Waste collector portal</title>
+        <style>
+            form input[type="text"],textarea{
+                color:white;
+                background-color: transparent;
+                border-bottom: 1px solid white;
+                border-top:none;
+                border-left:none;
+                border-right:none;
+                font-family: Arial, Helvetica, sans-serif;
+                height:50px;
+            }
+            form input[type="submit"]{
+                
+                border:none;
+                font-size:1rem;
+            }
+            ::placeholder{
+                color:white;
+                font-family: Arial, Helvetica, sans-serif;
+                font-size:1rem;
+            }
+        </style>
     </head>
     <body>
         <!--===== HEADER =====-->
@@ -52,20 +74,26 @@
             <!--===== HOME =====-->
             <section class="home" id="home">
                 <div class="home__container bd-grid">
-                    <!--div class="home__img">
+                    <div class="home__img">
                         <img src="img/img1.png" alt="" data-speed="-2" class="move">
                         <img src="img/img2.png" alt="" data-speed="2" class="move">
                         <img src="img/img3.png" alt="" data-speed="2" class="move">
                         <img src="img/img4.png" alt="" data-speed="-2" class="move">
                         <img src="img/img5.png" alt="" data-speed="-2" class="move">
-                    </div-->
+                    </div>
 
                     <div class="home__data">
-                        <h1 class="home__title">Welcome<br> to E-Waste <br> Collector portal</h1>
-                        <p class="home__description">Communicate with State-level recyclers <br> and track E-Waste bins<br>in your pincode.</p>
-                        <a href="#" class="home__button">Track Bins</a>
-                        <a href="#" class="home__button">Specify Shipment details</a>
-                        
+                        <h2>Specify shipment details if your request has been approved.</h2>
+                        <!--h3>Give a detailed description about the category and quantity of e-waste.</h3-->
+                        <form name="ship_form" action="col_shipments.php" method="post">
+                            <input type="text" name="rec_id" placeholder="Enter recycler ID." style="width:650px;" required><br>
+                            <br><textarea placeholder="Enter description." name="req_des" style="width:650px;" required></textarea><br>
+                            <br><input type="text" name="veh_type" placeholder="Enter vehicle type." style="width:650px;" required><br>
+                            <br><input type="text" name="veh_no" placeholder="Enter vehicle number." style="width:650px;" required><br>
+                            <br><input type="text" name="driver_name" placeholder="Enter driver name." style="width:650px;" required><br>
+                            <br><input type="text" name="driver_mno" placeholder="Enter driver mobile number." style="width:650px;" required><br>
+                            <br><input type="submit" class="home__button" name="submit"><br>
+                        </form>
                     </div>
                 </div>
             </section>
@@ -77,4 +105,26 @@
         <!--===== MAIN JS =====-->
         <script src="js/col_js.js"></script>
     </body>
+    <?php
+        if(isset($_POST['submit']))
+        {
+            $cid=$_SESSION['username'];
+            $rid=$_POST['rec_id'];
+            $des=$_POST['req_des'];
+            $vt=$_POST['veh_type'];
+            $vno=$_POST['veh_no'];
+            $dn=$_POST['driver_name'];
+            $dm=$_POST['driver_mno'];
+            $q="INSERT INTO SHIPMENT(`collector_id`,`recycler_id`,`description`,`vehicle_type`,`vehicle_no`,`driver_name`,`driver_mno`) VALUES('$cid','$rid','$des','$vt','$vno','$dn',$dm)";
+            $res=mysqli_query($con,$q);
+            if($res)
+            {
+                echo '<script>alert("Shipment details saved successfully.")</script>';
+            }
+            else
+            {
+                echo '<script>alert("Error occured! Try again.")</script>';
+            }
+        }
+    ?>
 </html>
--- js/col_js.js
+++ js/col_js.js
@@ -33,6 +33,7 @@ gsap.from('.nav__item', {opacity: 0, duration: 1, delay: 2.1, y: 30, stagger: 0.
 
 // HOME
 gsap.from('.home__title', {opacity: 0, duration: 1, delay:1.6, y: 30})
+gsap.from('.home__data', {opacity: 0, duration: 1, delay:1.7, y: 30})
 gsap.from('.home__description', {opacity: 0, duration: 1, delay:1.8, y: 30})
 gsap.from('.home__button', {opacity: 0, duration: 1, delay:2.1, y: 30})
 gsap.from('.home__img', {opacity: 0, duration: 1, delay:1.3, y: 30})
